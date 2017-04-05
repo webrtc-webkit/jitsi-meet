e4d1ea35347b0e45422f5b987685124da46bd837
Carlos Alberto Lopez Perez <clopez@igalia.com>
2017-04-05 15:32:20 +0200

    Customize deployment for jitsi-dev at igalia

--- config.js
+++ config.js
@@ -3,20 +3,21 @@
 var config = { // eslint-disable-line no-unused-vars
 //    configLocation: './config.json', // see ./modules/HttpConfigFetch.js
     hosts: {
-        domain: 'jitsi-meet.example.com',
+        domain: 'jitsi-dev.igalia.com',
         //anonymousdomain: 'guest.example.com',
         //authdomain: 'jitsi-meet.example.com',  // defaults to <domain>
-        muc: 'conference.jitsi-meet.example.com', // FIXME: use XEP-0030
+        muc: 'conference.jitsi-dev.igalia.com', // FIXME: use XEP-0030
         //jirecon: 'jirecon.jitsi-meet.example.com',
         //call_control: 'callcontrol.jitsi-meet.example.com',
-        //focus: 'focus.jitsi-meet.example.com', // defaults to 'focus.jitsi-meet.example.com'
+        bridge: 'jitsi-videobridge.jitsi-dev.igalia.com',
+        focus: 'focus.jitsi-dev.igalia.com',
     },
 //  getroomnode: function (path) { return 'someprefixpossiblybasedonpath'; },
 //  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
 //  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
-    clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
+    bosh: '//jitsi-dev.igalia.com/http-bind', // FIXME: use xep-0156 for that
+    clientNode: 'https://jitsi-dev.igalia.com', // The name of client node advertised in XEP-0115 'c' stanza
     //focusUserJid: 'focus@auth.jitsi-meet.example.com', // The real JID of focus participant - can be overridden here
     //defaultSipNumber: '', // Default SIP number
 
